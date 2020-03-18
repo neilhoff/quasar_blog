9b0789440736dfbb1ea73d229272ca50e7fbbb18
Neil Hoff <neilhoff@gmail.com>
2020-03-18 09:29:11 -0500

    Setup Quasar to build to /public and run the first build

--- quasar.conf.js
+++ quasar.conf.js
@@ -56,7 +56,9 @@ module.exports = function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-
+      distDir: 'public/',
+      publicPath: '/',
+      showProgress: true,
       // rtl: false, // https://quasar.dev/options/rtl-support
       // showProgress: false,
       // gzip: true,
